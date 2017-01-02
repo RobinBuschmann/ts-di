4e8eded6859bfb0d7ba2ae5676ea8728bafe1308
KostiaTretiak <ktretiak.in.ua@gmail.com>
2017-01-02 20:10:04 +0200

    Reduce export utils

--- src/util.ts
+++ src/util.ts
@@ -1,23 +1,23 @@
 // A bunch of helper functions.
 
-function isUpperCase(char: any)
+export function isUpperCase(char: any)
 {
   return char.toUpperCase() === char;
 }
 
-function isFunction(value: any)
+export function isFunction(value: any)
 {
   return typeof value === 'function';
 }
 
 
-function isObject(value: any)
+export function isObject(value: any)
 {
   return typeof value === 'object';
 }
 
 
-function toString(token: any)
+export function toString(token: any)
 {
   if (typeof token === 'string')
   {
@@ -37,11 +37,11 @@ function toString(token: any)
   return token.toString();
 }
 
-let ownKeys =
+export let ownKeys =
 (
   this
   && this.Reflect
-  && Reflect.ownKeys ? Reflect.ownKeys : function ownKeys(O: any)
+  && Reflect.ownKeys ? Reflect.ownKeys : function (O: any)
   {
     let keys = Object.getOwnPropertyNames(O);
 
@@ -51,12 +51,3 @@ let ownKeys =
     return keys;
   }
 );
-
-
-export {
-  isUpperCase,
-  isFunction,
-  isObject,
-  toString,
-  ownKeys
-};
